32e231c78b41356565c371183636195f65d01a6e
Alan Charles <alanjcharles14@gmail.com>
2018-06-07 16:04:38 -0600

    Add button styling

--- src/Components/ScheduleCard/ScheduleCard.js
+++ src/Components/ScheduleCard/ScheduleCard.js
@@ -5,9 +5,9 @@ const ScheduleCard = () => {
     return (
         <div className="schedule-card">
             <div className="schedule-text">
-                <h1>Schedule A Free Consultation Today</h1>
-                <button>Schedule</button>
+                <h1 className="schedule-intro">Schedule A Free Consultation Today</h1>
             </div>
+            <button className="schedule-button">Schedule</button>
         </div>
     )
 }
