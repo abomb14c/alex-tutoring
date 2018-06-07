3b47f1dd31a56695b51b3b478b9fb0c6f90cda15
Alan Charles <alanjcharles14@gmail.com>
2018-06-07 12:37:34 -0600

    add hero component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Link } from 'react-router-dom'
 import Navigation from '../Navigation/Navigation'
+import Mission from '../Mission/Mission'
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
         <header className="App-header">
           <Navigation />
         </header>
+        <Mission />
       </div>
     );
   }
---
+++ src/Components/Mission/Mission.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './mission.css'
+
+const Mission = () => {
+    return (
+        <div className="mission-container">
+            <div className="mission">
+                <h1 className="mission-hero">Los Angeles In-Home and Nationwide Online Tutoring </h1>
+                <h5 className="mission-secondary">Bespoke, data-driven ACT & SAT prep </h5>
+            </div>
+        </div>
+    )
+}
+
+export default Mission;
